73a9fdd136447467683849f5a9d52a23b56affb8
unknown <303Laerer@FUV-303LAERER.studnett.local>
2015-11-11 21:16:31 +0100

    Half way version

---
+++ game.py
@@ -0,0 +1,61 @@
+import turtle
+import threading
+from time import sleep
+from random import randrange
+
+class Character(turtle.Turtle): #note capiptal letter in beginning of class name
+    def __init__(self, space):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.space = space # contains 2 x,y coord defining world space/game board, similar to turtle.Vec2D
+        pass
+
+    def __repr__(self):
+        return ""
+
+    def canmove(self, x, y):
+        "function checks if character is allowed to move within space, returns false if trying to move without of bounds (self.space)"
+        # write function for checking if can move within space
+        return -1 * self.space[0] < x < self.space[0] and -1 * self.space[1] < y < self.space[1]
+
+    def move(self, x, y):
+        if self.canmove(x, y):
+            turtle.tracer(50)
+            self.goto(x, y)
+            turtle.tracer(1)
+            return True
+        else:
+            return False
+
+class Pc(Character):
+    def __init__(self, helse, space):
+        Character.__init__(self, space)
+        self.hitpoints = helse
+        self.shape("playership.gif")
+    def mistliv(self):
+        self.hitpoints = self.hitpoints - 1
+
+
+class NPC(Character):
+    def getxyfromfwd(self, distance):
+        # calculate coordinates from distance given setheading
+        self.heading()
+        return [0,0]
+
+    def movement(self):
+        while True:
+            sleep(self.fart)
+
+    def __init__(self, space, radius, fart=250):
+        super(NPC, self).__init__(space) # another way of Character.__init__(self)
+        turtle.tracer(50)
+        self.shape("Alienship.gif")
+        self.space = space
+        self.radius = radius
+        self.fart = fart
+        self.setheading(randrange(360))
+        self.t = threading.Thread(target=self.movement)
+        self.t.start()
+        turtle.tracer(1)
+
+
---
+++ spaceinvaders.py
@@ -0,0 +1,41 @@
+import turtle
+import game
+
+import configparser
+import sys
+import threading
+import time
+from random import randrange
+
+def loadkeystoturtle(targetturtle):
+    config = configparser.ConfigParser()
+    config.optionxform=str  # preserve case
+    config.read('config.ini')
+    for section in config.sections():
+        if section.lower() == 'keys':
+            for key in config[section]:
+                name = config['Keys'][key]
+                obta = sys.modules[__name__]
+                if hasattr(obta, name):
+                    func = getattr(obta, name) # a function with no arguments or None
+                    targetturtle.onkeypress(func, key)
+
+def mainthread():
+    while True:
+        time.sleep(250)
+
+if __name__ == '__main__':
+    t = threading.Thread(target=mainthread)
+    # t.daemon = True
+    t.start()
+
+    space = turtle.screensize()
+    npcradius = 20
+    npcs = []
+    turtle.register_shape("Alienship.gif", shape = None)
+    turtle.register_shape("playership.gif", shape = None)
+    for i in range(0, 5):
+        npcs.append(game.NPC(space, npcradius))
+
+
+    turtle.mainloop()
