bba5479c4b2902553412534cf345ce32f57805fe
303PC06 <303PC06@studnett.local>
2015-11-18 19:54:21 +0100

    lagt til metoder for highscore

---
+++ highscore.py
@@ -0,0 +1,24 @@
+
+def playername():
+    #får navn fra spiller og legger det til i highscore listen
+    #returnerer spillernavn
+    pass
+
+def starttid():
+    #starter en timer
+    pass
+
+def stoptid():
+    #stopper timeren
+    pass
+
+def lastinnhighscorefil(filnavn):
+    #henter en fil med highscore
+    pass
+
+def lagretilhighscore(filnavn):
+    #Lagrer highscoren i en fil
+    pass
+
+
+
